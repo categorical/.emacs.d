dc9d307e22b3a7477c3baa32506878752001fd57
dawei <dawei.chen@hotmail.co.uk>
2021-11-16 04:05:01 +0000

    update

--- codestyles.el
+++ codestyles.el
@@ -5,3 +5,14 @@
         tab-width 4
         indent-tabs-mode t)))
 
+
+
+(add-hook 'sh-mode-hook (lambda()
+    (setq sh-basic-offset 4
+        ;;tab-width 4
+        indent-tabs-mode nil
+        comment-padding 0
+        comment-start "#")))
+
+
+

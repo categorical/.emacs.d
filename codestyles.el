3de0799ddd8aca9a87ef98f3cfc93ccabb9511c3
dawei <dawei.chen@hotmail.co.uk>
2021-11-16 10:37:19 +0000

    update

--- codestyles.el
+++ codestyles.el
@@ -9,10 +9,17 @@
 
 (add-hook 'sh-mode-hook (lambda()
     (setq sh-basic-offset 4
-        ;;tab-width 4
         indent-tabs-mode nil
+        ;; C-q tab width
+        tab-width 4
+        ;; disables nested indentation rules after line continuation
+        sh-indent-after-continuation 'always
+        ;; heredoc/text-mode: tab aligns to a tab stop (M-i)
+        tab-always-indent nil
         comment-padding 0
         comment-start "#")))
+
+
 
 
 
